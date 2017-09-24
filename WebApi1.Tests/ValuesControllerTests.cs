aaf17a6e356f85bd535fab607c9ea941b7d9e505
Hjusein Tjurkmen <hjusein@dynamosoftware.com>
2017-09-24 23:28:19 +0300

    dotnet testing example

---
+++ WebApi1.Tests/ValuesControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApi1.Controllers;
+
+namespace WebApi1.Tests
+{
+    public class ValuesControllerTests
+    {
+        [TestCase]
+        public void GetTest() {
+            ValuesController contr = new ValuesController();
+            var result = contr.Get(1);
+            Assert.AreEqual("value", result);
+        }
+    }
+}
